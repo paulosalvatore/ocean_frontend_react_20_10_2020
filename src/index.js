c7278c2da631e731638d6ad01a884d6a69bcd8b5
Paulo Salvatore <paulo@salvatore.tech>
2020-10-20 14:38:46 -0300

    Página inicial criada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Game(props) {
+    return <div>Olá, mundo!</div>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // Componente
+    <Game/>,
+    // Em um elemento
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
