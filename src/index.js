e34f266aa5f327a99dfe092a8cdc14d494c3e395
Paulo Salvatore <paulo@salvatore.tech>
2020-10-20 15:52:41 -0300

    Renderização dos squares através da função renderSquare, exibindo o índice de cada square

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Square(props) {
+    return <button className="square">
+        {props.value}
+    </button>;
+}
+
 class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>
+    }
+
     render() {
-        return <div>Board</div>;
+        return <div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+        </div>;
     }
 }
 
